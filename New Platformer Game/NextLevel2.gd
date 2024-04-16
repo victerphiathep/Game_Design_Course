bb08af9820307b6929a5dd69f8e322b9bb46901b
Victer <victerphiathep@gmail.com>
2024-04-16 11:40:09 -0400

    Starting Alpha Build

--- New Platformer Game/NextLevel2.gd
+++ New Platformer Game/NextLevel2.gd
@@ -3,7 +3,7 @@ extends Area2D
 func _on_body_entered(body):
     if body.is_in_group("Player"):
         var current_scene_file = get_tree().current_scene.scene_file_path
-        var world_3_village_path = "res://world_3_village.tscn"
+        var world_3_village_path = "res://world_4_forest.tscn"
         get_tree().change_scene_to_file(world_3_village_path)
         print("Collided Village!")
         
