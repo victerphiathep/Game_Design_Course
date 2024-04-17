63c6664812492b4dc9893974b9044a66ec030cb3
Victer <victerphiathep@gmail.com>
2024-04-16 21:37:28 -0400

    Alpha Build 1.2
    Animations + Tiles + Traps + Audio

---
+++ New Platformer Game/checkpoint.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var checkpoint = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if not checkpoint:
+		get_node("Checkpoint_Animation").play("Checkpoint")
+		checkpoint = true
