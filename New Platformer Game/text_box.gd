c3c66d010decd6234cfe6b8229aac77d1c8c6b30
Victer <victerphiathep@gmail.com>
2024-05-09 22:13:02 -0400

    Final Build 1
    Submitted Build

--- New Platformer Game/text_box.gd
+++ New Platformer Game/text_box.gd
@@ -4,19 +4,19 @@ extends CanvasLayer
 @onready var dialogue_panel = $TextboxContainer/DialoguePanel
 
 func _ready():
-    # Initially hide the dialogue panel
-    dialogue_panel.visible = false
-    # Call the function to show dialogue when the game starts
-    show_dialogue("Where am I..?", 5.0)  # Display the text for 5 seconds
+	# Initially hide the dialogue panel
+	dialogue_panel.visible = false
+	# Call the function to show dialogue when the game starts
+	show_dialogue("Welcome to Eldritch Fields. Reach the last dungeon stage to beat the game!", 5.0)  # Display the text for 5 seconds
 
 func show_dialogue(text: String, duration: float):
-    dialogue_panel.get_node("Label").text = text  # Update the Label text
-    dialogue_panel.visible = true  # Make the dialogue panel visible
-    # Optionally hide the dialogue after a duration
-    await get_tree().create_timer(duration).timeout
-    dialogue_panel.visible = false
+	dialogue_panel.get_node("Label").text = text  # Update the Label text
+	dialogue_panel.visible = true  # Make the dialogue panel visible
+	# Optionally hide the dialogue after a duration
+	await get_tree().create_timer(duration).timeout
+	dialogue_panel.visible = false
 
 func _input(event):
-    # Hide the dialogue if any key is pressed
-    if event is InputEventKey and event.pressed:
-        dialogue_panel.visible = false
+	# Hide the dialogue if any key is pressed
+	if event is InputEventKey and event.pressed:
+		dialogue_panel.visible = false
